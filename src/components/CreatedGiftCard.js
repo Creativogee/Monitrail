2e7c6025492a41e9a9102b318f4d61e670416481
Creativogee <omowole.gbenga@gmail.com>
2020-12-25 17:18:35 +0100

    New feature: save giftcard as pdf

--- src/components/CreatedGiftCard.js
+++ src/components/CreatedGiftCard.js
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
 import QRCode from 'react-qr-code';
+import html2Canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
+
 import { GlobalContext } from '../context/GlobalState';
 
 const CreatedGiftCard = ({ newGiftCard }) => {
@@ -10,9 +13,27 @@ const CreatedGiftCard = ({ newGiftCard }) => {
   const amount = transactions[0].voucher.amount;
   const retailer = transactions[0].voucher.retailer;
 
+  //New Feature: Saves voucher to the user's device
+
+  function saveVoucher(name) {
+    const canvas = document.querySelector('#voucher');
+    html2Canvas(canvas, {
+      scrollY: -window.scrollY,
+      scrollX: -window.scrollX,
+      scale: 2,
+    }).then((canvas) => {
+      let dataURL = canvas.toDataURL('image/jpeg', 2.0);
+      const voucher = new jsPDF({
+        unit: 'px',
+        format: [300, 355],
+      });
+      voucher.addImage(dataURL, 'JPEG', 0, 0, 285, 355);
+      voucher.save(`${name}.pdf`);
+    });
+  }
   return (
     <>
-      <div className='voucher'>
+      <div id='voucher' className='voucher'>
         <div className='voucher_info'>
           <span>{retailer}</span>
           <span>$ {amount}</span>
@@ -21,7 +42,12 @@ const CreatedGiftCard = ({ newGiftCard }) => {
           <QRCode value={giftCardJSON} />
         </div>
       </div>
-      <button className='btn'>SAVE</button>
+      <button
+        onClick={() => saveVoucher(`${retailer}-${amount}-voucher`)}
+        className='btn'
+      >
+        SAVE
+      </button>
     </>
   );
 };
